ec38b5f4fbc9af9b91779dc9d748a6b5e448c4b6
Kate <kate.tolstaya@gmail.com>
2019-05-10 10:23:18 -0400

    compose

--- compose/combine_models.py
+++ compose/combine_models.py
@@ -1,93 +1,75 @@
 import numpy as np
+import scipy.sparse.linalg
 import pickle
 from algs.knaf import KNAFModel
 from itertools import combinations
+
 try:
     from configparser import ConfigParser
 except ImportError:
     from ConfigParser import ConfigParser
 
-def softmax(a,b):
-	return np.exp(a) / (np.exp(a) + np.exp(b))
+
+def softmax(a, b):
+    return np.exp(a) / (np.exp(a) + np.exp(b))
+
 
 def scores(models, x, ind):
+    scores = np.zeros((len(ind),))
+    j = 0
+    for i in ind:
+        scores[j] = np.sum(models[i].vpl.kernel.f(x, models[i].vpl.D), axis=1)
+        j = j + 1
 
-	scores = np.zeros((len(ind),))
-	j = 0
-	for i in ind:
-		scores[j] = np.sum(models[i].vpl.kernel.f(x, models[i].vpl.D), axis=1)
-		j = j+1
+    return ind[np.argmax(scores)]
 
-	return ind[np.argmax(scores)]
 
 # models to combine
-#model_names = [ "robot_results/rob14_model54.txt", "robot_results/rob15_model25.txt", "exp18/rob16_model74.txt"] 
+# model_names = [ "robot_results/rob14_model54.txt", "robot_results/rob15_model25.txt", "exp18/rob16_model74.txt"]
 
-model_names = ["robot_results/exp20/rob20_model23.txt", "robot_results/exp21/rob21_model63.txt",  "robot_results/exp23/rob23_3_model3.txt", "robot_results/exp22/rob22_model50.txt"]
+model_names = ["robot_results/exp20/rob20_model23.txt", "robot_results/exp21/rob21_model63.txt",
+               "robot_results/exp23/rob23_3_model3.txt", "robot_results/exp22/rob22_model50.txt"]
 
 # load models
 models = list()
 n_centers = 0
 for s in model_names:
-	m = pickle.load(open(s,'rb'))
-	models.append(m)
-	n_centers = n_centers + np.shape(m.vpl.D)[0]
-	print np.shape(m.vpl.D)[0]
+    m = pickle.load(open(s, 'rb'))
+    models.append(m)
+    n_centers = n_centers + np.shape(m.vpl.D)[0]
+    print np.shape(m.vpl.D)[0]
 
-# # assemble all dictionary points
-# centers = np.zeros((n_centers,5))
-# c_model = np.zeros((n_centers,),dtype=int)
-# mi = 0
-# for i in range(0,len(models)):
-# 	l = np.shape(models[i].vpl.D)[0]
-# 	centers[np.arange(mi, mi+l),:] = models[i].vpl.D
-# 	c_model[np.arange(mi, mi+l)] = i
-# 	mi=mi+l
+# assemble all dictionary points
+centers = np.zeros((n_centers, 5))
+c_model = np.zeros((n_centers,), dtype=int)
+ys = np.zeros((n_centers, 3))
+mi = 0
+for i in range(0, len(models)):
+    l = np.shape(models[i].vpl.D)[0]
+    centers[np.arange(mi, mi + l), :] = models[i].vpl.D
+    c_model[np.arange(mi, mi + l)] = i
+    ys[np.arange(mi, mi + l), :] = models[i].vpl(models[i].vpl.D)
+    mi = mi + l
 
-# # set up new model
-# fname = 'cfg/knaf_robot.cfg'
-# config = ConfigParser()
-# with open(fname, 'r') as f:
-#     config.read_file(f)
+# set up new model
+fname = 'cfg/knaf_robot.cfg'
+config = ConfigParser()
+with open(fname, 'r') as f:
+    config.read_file(f)
+model = KNAFModel(5, 1, config[config.default_section])
 
-# flist = open('combos/list.txt','wb')
-# flist.write(str(model_names) + '\n')
+KDD = np.zeros((n_centers, n_centers))
 
-# n_save = 0
-# for n_c in range(1,len(models)+1):
+for i in range(0, n_centers):
+    KDD[i, :] = model.vpl.kernel.f(centers[i, :], centers)
 
-# 	# for each combo, let's make a model
-# 	for ind in combinations(range(0,len(models)),n_c):	
-# 		model = KNAFModel(5, 1, config[config.default_section])
+weights = np.linalg.lstsq(KDD, ys)[0]
 
-# 		# analyze points in random order
-# 		for i in np.random.permutation(np.shape(centers)[0]): #range(0,np.shape(centers)[0]):
+model.vpl.append(centers, weights)
+print np.shape(model.vpl.D)
 
-# 			if c_model[i] in ind: # if this dictionary point is from a policy in the combo
-# 				# get center i and its model
-# 				x = centers[i,:]
-# 				model_ = models[c_model[i]]
+model.vpl.prune(model.eps)
 
-# 				# density scores at x
-# 				#score_ = np.sum(model_.vpl.kernel.f(x, model_.vpl.D), axis=1)
-# 				#score = np.sum(model.vpl.kernel.f(x, model.vpl.D), axis=1)
+print np.shape(model.vpl.D)
 
-# 				# update model
-# 				if scores(models, x, ind) == c_model[i]: #> score:
-
-# 					model.vpl.append(x, (model_.vpl(x) - model.vpl(x)))
-
-# 		model.vpl.prune(model.eps)
-
-# 		# save model
-# 		print (np.shape(model.vpl.D))
-# 		pickle.dump(model,open("combos/policy" + str(n_save) + ".txt",'wb'))
-# 		flist.write("combos/policy" + str(n_save) + ".txt, " + str(ind) + '\n')
-# 		n_save = n_save + 1
-
-
-
-
-
-
-
+pickle.dump(model, open("lst.txt", 'wb'))
---
+++ compose/model.py
@@ -0,0 +1,215 @@
+import copy, json, random
+import numpy as np
+from corerl.core import ScheduledParameter
+from corerl.function import KernelRepresentation
+import pdb
+import pickle
+import csv
+import matplotlib.pyplot as plt
+from enum import Enum
+
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+
+
+
+
+class ModelParameters():
+    # Learning rates
+    eta = 0.5 #0.1
+    beta1 = 0.5
+    beta2 = 0.5
+    # Regularization
+    lossL =  1e-6
+    # Representation error budget
+    eps = 1.0
+
+    dt = 100
+    #mu = 455.0
+    #sigma = 200
+    mu = 0.0
+    sigma = 100 #200 #100 #50
+    fname = 'cfg/kadam.cfg'
+    config = ConfigParser()
+    with open(fname, 'r') as f:
+        config.read_file(f)
+    config = config[config.default_section]
+
+class GradType(Enum):
+    SGD = 0
+    MOM = 1
+    VAR = 2
+    MOMENTUM = 3
+    DELTA = 4
+
+class Model:
+
+
+    def __init__(self, indim, outdim, grad_type):
+
+
+        self.f = KernelRepresentation(indim, outdim, ModelParameters.config)
+        self.indim = indim
+        self.outdim = outdim
+        self.grad_type = grad_type
+        self.delta = 0
+
+    def model_error(self):
+        return 0.5 * ModelParameters.lossL * self.f.normsq()
+
+    def predict(self, x):  # Predict the Q function values for a batch of states.
+        return self.f(x)
+
+    def predictOne(self, x):  # Predict the Q function values for a single state.
+        return self.f(np.reshape(x,(1,-1)))[0]
+
+    def ucb(self, x):  # Predict the Q function values for a single state.
+        return np.abs(self.mom(x)) + np.sqrt(self.var(x))
+
+    def val(self, x):
+        return self.predictOne(x)[0] + ModelParameters.mu
+
+    def mom(self, x):
+        return self.predictOne(x)[1] + 100.0
+        
+    def var(self, x):
+        return max(0, self.predictOne(x)[2]  + ModelParameters.sigma)
+
+    def train(self, sample):
+
+        x,y = sample
+        grad = (self.val(x)-y)
+        grad_sq = grad**2
+         
+        # V gradient
+        W = np.zeros((3,))
+        
+        if self.grad_type ==GradType.SGD: # simple SGD
+            W[0] =  -ModelParameters.eta * grad
+        # elif self.grad_type == GradType.MOM:
+        #     W[0] =  -ModelParameters.eta * grad / (np.sqrt(self.var(x) + ModelParameters.eta**2))
+        #     W[2] = (-ModelParameters.beta2 * self.var(x) + ModelParameters.beta2 * grad_sq)
+        elif self.grad_type == GradType.MOM:
+            # W[0] =  -ModelParameters.eta * self.mom(x) #/ (np.sqrt(self.var(x) + ModelParameters.eta**2))
+            # W[1] = (-ModelParameters.beta1 * self.mom(x) + ModelParameters.beta1 * grad) 
+            ##########################
+            W[0] =  -ModelParameters.eta * self.mom(x) #/ (np.sqrt(self.var(x) + ModelParameters.eta**2))
+            W[1] = (-ModelParameters.beta1 * self.mom(x) + ModelParameters.beta1 * grad) 
+            #W[2] = (-ModelParameters.beta2 * self.var(x) + ModelParameters.beta2 * grad_sq)
+        elif self.grad_type == GradType.VAR:
+            grad_var = (grad-self.mom(x))**2
+            #print(grad_var)
+            W[0] =  -ModelParameters.eta * self.mom(x) #/ (np.sqrt(self.var(x) + ModelParameters.eta**2)) #/ np.sqrt(self.var(x) + ModelParameters.eta) 
+            W[1] = (-ModelParameters.beta1 * self.mom(x) + ModelParameters.beta1 * grad) 
+            W[2] = (-ModelParameters.beta2 * self.var(x) + ModelParameters.beta2 * grad_var) 
+        elif self.grad_type == GradType.MOMENTUM:
+            W[0] =  -ModelParameters.eta * self.mom(x) / (np.sqrt(self.var(x) + ModelParameters.eta**2))
+            W[1] = (-ModelParameters.beta1 * self.mom(x) + ModelParameters.beta1 * grad) 
+            W[2] = (-ModelParameters.beta2 * self.var(x) + ModelParameters.beta2 * grad_sq) 
+        elif self.grad_type == GradType.DELTA:
+            W[0] =  -ModelParameters.eta * self.delta
+            self.delta = self.delta + (-ModelParameters.beta1 * self.delta + ModelParameters.beta1 * grad) 
+        else:
+            print('error')
+        
+        # Gradient step
+        self.f.shrink(1. - ModelParameters.lossL)
+        self.f.append(np.array(x), np.reshape(W, (1, -1)))
+        # Prune
+        self.f.prune(ModelParameters.eps)
+        
+        return (grad_sq/2, len(self.f.D))
+        
+    def loss(self, sample):
+        x, y = sample
+        return 0.5 * (self.val(x)-y)**2
+
+    def point_density(self, x):
+        return np.sum(self.f.kernel.f(x, self.f.D))
+        
+    def compose(f1, f2): #static function
+
+        #f = KernelRepresentation(4, 3, config)
+        f = Model(f1.indim, f1.outdim, f1.grad_type )
+        d = np.vstack([f1.f.D, f2.f.D]) 
+        
+        thresh = np.shape(f1.f.D)[0]
+        
+        W = np.zeros((3,))
+        for i in np.random.permutation(np.shape(d)[0]):
+            x = d[i,:]
+            if f.grad_type == GradType.SGD:
+                if f1.point_density(x) >= f2.point_density(x) and i < thresh: # reciprocal here so larger is better
+                    W[1] = -f.mom(x) + f1.mom(x)
+                    W[2] = -f.var(x) + f1.var(x)
+                    W[0] = -f.val(x) + f1.val(x)
+                    f.f.append(np.array(x), np.reshape(W, (1, -1)))
+                elif f1.point_density(x) < f2.point_density(x) and i >= thresh:
+                    W[1] = -f.mom(x) + f2.mom(x)
+                    W[2] = -f.var(x) + f2.var(x)
+                    W[0] = -f.val(x) + f2.val(x)
+                    f.f.append(np.array(x), np.reshape(W, (1, -1)))
+            elif f.grad_type == GradType.MOM :
+                if f1.mom(x) <= f2.mom(x) and i < thresh: # reciprocal here so larger is better
+                    W[1] = -f.mom(x) + f1.mom(x)
+                    W[2] = -f.var(x) + f1.var(x)
+                    W[0] = -f.val(x) + f1.val(x)
+                    f.f.append(np.array(x), np.reshape(W, (1, -1)))
+                elif f1.mom(x) > f2.mom(x) and i >= thresh:
+                    W[1] = -f.mom(x) + f2.mom(x)
+                    W[2] = -f.var(x) + f2.var(x)
+                    W[0] = -f.val(x) + f2.val(x)
+                    f.f.append(np.array(x), np.reshape(W, (1, -1)))
+            # elif f.grad_type == GradType.VAR:
+            #     if f1.var(x) <= f2.var(x) and i < thresh: # reciprocal here so larger is better
+            #         W[1] = -f.mom(x) + f1.mom(x)
+            #         W[2] = -f.var(x) + f1.var(x)
+            #         W[0] = -f.val(x) + f1.val(x)
+            #         f.f.append(np.array(x), np.reshape(W, (1, -1)))
+            #     elif f1.var(x) > f2.var(x) and i >= thresh:
+            #         W[1] = -f.mom(x) + f2.mom(x)
+            #         W[2] = -f.var(x) + f2.var(x)
+            #         W[0] = -f.val(x) + f2.val(x)
+            #         f.f.append(np.array(x), np.reshape(W, (1, -1)))
+            elif  f.grad_type == GradType.VAR:
+                if (f1.ucb(x)) <= f2.ucb(x) and i < thresh: # reciprocal here so larger is better
+                    W[1] = -f.mom(x) + f1.mom(x)
+                    W[2] = -f.var(x) + f1.var(x)
+                    W[0] = -f.val(x) + f1.val(x)
+                    f.f.append(np.array(x), np.reshape(W, (1, -1)))
+                elif f1.ucb(x) > f2.ucb(x) and i >= thresh:
+                    W[1] = -f.mom(x) + f2.mom(x)
+                    W[2] = -f.var(x) + f2.var(x)
+                    W[0] = -f.val(x) + f2.val(x)
+                    f.f.append(np.array(x), np.reshape(W, (1, -1)))
+        return f
+
+def train_model(reader, grad_type=GradType.MOM):
+    np.random.seed(0)
+    f = Model(4, 3, grad_type)
+
+    losses = []
+
+    for i, row in enumerate(reader):
+        sample = (np.array([float(row[0]), float(row[1]), float(row[2]), float(row[3])]), float(row[4]))
+        (loss, model) =  f.train(sample)
+        losses.append(loss)        
+    return f
+    
+def test_model(f, reader):
+    np.random.seed(2)
+    n = 0
+    temp_loss = 0
+    for i, row in enumerate(reader):
+        sample = (np.array([float(row[0]), float(row[1]), float(row[2]), float(row[3])]), float(row[4])) # 
+        temp_loss = temp_loss + f.loss(sample)
+        n = n + 1
+    return temp_loss/n
+    
+
+
+
+
